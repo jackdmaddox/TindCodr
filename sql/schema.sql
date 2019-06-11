77b91fdb6be1f71aeeb3e953db48ebbc129b471a
Brodie Brock <brodiebrock@gmail.com>
2019-06-11 15:58:28 -0400

    made a schema.sql and seed.sql file for seed data. also made conn.js file.

---
+++ sql/schema.sql
@@ -0,0 +1,24 @@
+create table users (
+  id serial primary key,
+  users_first_name varchar(100), 
+  users_last_name varchar(100),
+  users_email varchar(100), 
+  users_password varchar(500),
+  users_city varchar(100)
+);
+
+create table project (
+  id serial primary key,
+  project_title varchar(100),
+  project_start varchar(100),
+  project_summary varchar(100),
+  project_url varchar(100),
+  project_open varchar(100)
+);
+
+create table comments (
+  id serial primary key,
+  comments_content varchar(500),
+  comments_users_id integer references users(id),
+  comments_project_id integer references project(id)
+  );
