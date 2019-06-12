869910c78325be4125aecdafe4cc1a9851eb08f5
Thomas Nguyen <thomasnguyen@Thomass-MacBook-Pro.local>
2019-06-12 12:34:37 -0400

    updated users

--- models/users-models.js
+++ models/users-models.js
@@ -2,31 +2,30 @@ const db = require('./conn-model'),
     bcrypt = require('bcryptjs');
 
 class User {
-    constructor(id, project_title, project_start, project_summary, project_url, project_open, password) {
+    constructor(id, users_first_name, users_last_name, users_email, users_password, users_city, password) {
         this.id = id;
-        this.project_title = project_title;
-        this.project_start = project_start;
-        this.project_summary = project_summary;
-        this.project_url = project_url;
-        this.project_open = project_open;
-        this.password = password;
+        this.users_first_name = users_first_name;
+        this.users_last_name = users_last_name;
+        this.users_email = users_email;
+        this.users_password = users_password;
+        this.users_city = users_city;   
     }
 
     async checkPassword(hashedPassword) {
         //syntact: bcrypt.comapresynce(part one, part two)
         //first argument is what user puts in form, second is hashed password. Returns true or false
-        return bcrypt.compareSync(this.password, hashedPassword);
+        return bcrypt.compareSync(this.users_password, hashedPassword);
     }
 
     async save() {
         try {
             const response = await db.one(
                 `insert into users
-                    (project_title, project_start, project_summary, project_url, project_open, password)
+                    (users_first_name, users_last_name, users_email, users_password, users_city)
                 values
                     ($1, $2, $3, $4, $5, $6)
                 returning id
-                `, [this.project_title, this.project_start, this.project_summary, this.project_url, this.project_open, this.password]);
+                `, [this.users_first_name, this.users_last_name, this.users_email, this.users_password, this.users_city]);
             console.log('user was created with id:', response.id);
             return response;
         } catch (err) {
@@ -37,17 +36,17 @@ class User {
     async login() {
         try {
             const response = await db.one(`
-                    select id, project_title, project_start, password
+                    select id, users_first_name, users_last_name, users_password
                         from users
-                    where project_summary = $1`, [this.project_summary]);
-            console.log('hash is', response.password);
-            const isValid = await this.checkPassword(response.password);
+                    where users_email = $1`, [this.users_email]);
+            console.log('hash is', response.users_password);
+            const isValid = await this.checkPassword(response.users_password);
             console.log('is it valid?', isValid);
             if (!!isValid) { /// if isvalid === absolutely true
                 // then we destructure values we want from the response
-                const { project_title, project_start, id } = response;
+                const { users_first_name, users_last_name, users_city, id } = response;
                 //this lines will return the isValid, first name, last name and user id
-                return { isValid, project_title, project_start, user_id: id }
+                return { isValid, users_first_name, users_last_name, users_city, user_id: id }
             } else {
                 // otherwise just return the false isvalid
                 return { isValid }
