26d9223bbd3e73e3cc84f98b30cdd445bb32f3bf
Brodie Brock <brodiebrock@gmail.com>
2019-06-13 13:22:28 -0400

    conn.js

--- models/conn-model.js
+++ models/conn-model.js
@@ -6,11 +6,7 @@ const pgp = require('pg-promise')({
 const options = {
     host: 'localhost',
     database: 'tindcodr',
-<<<<<<< HEAD
     password: 'Fiddle123'
-=======
-    user: 'nguyent'
->>>>>>> 895b15608a38f48e3992d3b593d3c7b46bd6d290
 };
 const db = pgp(options);
 module.exports = db;
