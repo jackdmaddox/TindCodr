85086fde0c555033dbdbe11e54def69819b425a1
Jack Maddox <jackdmaddox@gmail.com>
2019-06-18 12:24:34 -0400

    final css changes and added new project page

--- sql/schema.sql
+++ sql/schema.sql
@@ -8,11 +8,14 @@ create table users (
   users_about_me varchar(1000)
 );
 
+alter table users
+add column users_image_url varchar(500);
+
 create table projects (
   id serial primary key,
   project_title varchar(100),
   project_start varchar(100),
-  project_summary varchar(100),
+  project_summary varchar(2000),
   project_url varchar(100),
   project_open varchar(100),
   project_users_id integer references users(id)
--- sql/seed.sql
+++ sql/seed.sql
@@ -52,6 +52,33 @@ insert into projects
     (project_title, project_start, project_summary, project_url, project_open, project_users_id)
 VALUES
     ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 1);
+    
+insert into projects
+    (project_title, project_start, project_summary, project_url, project_open, project_users_id)
+VALUES
+    ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 2);
+
+insert into projects
+    (project_title, project_start, project_summary, project_url, project_open, project_users_id)
+VALUES
+    ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 3);
+
+insert into projects
+    (project_title, project_start, project_summary, project_url, project_open, project_users_id)
+VALUES
+    ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 4);
+
+insert into projects
+    (project_title, project_start, project_summary, project_url, project_open, project_users_id)
+VALUES
+    ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 5);
+
+insert into projects
+    (project_title, project_start, project_summary, project_url, project_open, project_users_id)
+VALUES
+    ('tindcodr', 'friday', 'group project 2 to take over the world!', 'bigurl here', 'Open', 1);
+
+
 
 insert into comments
     (comments_content, comments_users_id, comments_project_id)
